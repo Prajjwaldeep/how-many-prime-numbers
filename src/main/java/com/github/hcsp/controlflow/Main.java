c663ef91acd6f8e62a63ae478f03bd26dfe83f09
Prajjwaldeep <76683020+Prajjwaldeep@users.noreply.github.com>
2023-07-29 17:01:04 +0530

    Update Main.java
    Code quality and readability updated

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -1,19 +1,31 @@
-package com.github.hcsp.controlflow;
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(howManyPrimeNumbers(100));
+public class PrimeNumbersinRange {
+    public static boolean isPrime(int n)
+{
+    int cnt=0;
+    for(int i=1; i<=n; i++)
+    {
+        if(n%i==0)
+        {
+            cnt++;
+        }
     }
-
-    /**
-     * 给定一个数字n，返回1到n之间的质数（素数）个数，不包括n本身。
-     *
-     * <p>例如，n=5，返回2，因为1到5之间有2个质数：2和3。
-     *
-     * <p>提示：对于正整数n，如果用2到Math.sqrt(n)+1之间的所有整数去除，均无法整除，则n为质数。
-     *
-     * @param n 给定的数字
-     * @return 1到n之间(不包括n)质数的个数
-     */
-    public static int howManyPrimeNumbers(int n) {}
+    return cnt==2;
+}
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        System.out.print("Enter the ending range : ");
+        int eR=in.nextInt();
+        System.out.println();
+        for(int i=1; i<=eR; i++)
+        {
+            if(isPrime(i))
+            {
+                System.out.print(i+" ");
+            }
+        }
+    }
 }
